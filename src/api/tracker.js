4d575b84713af9ffcebc0deaac851ea719a074a7
Michael Knutson <mrknutson44@gmail.com>
2022-03-19 14:12:58 -0800

    asking permission to track

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://dce7-216-67-123-95.ngrok.io",
+  baseURL: "http://fefb-216-67-123-95.ngrok.io",
 });
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,11 +1,33 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
+import { SafeAreaView } from "react-navigation";
+import Map from "../components/Map";
+import { requestForegroundPermissionsAsync } from "expo-location";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  });
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>TrackCreate Screen</Text>
-    </View>
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text h2>Create Track</Text>
+      <Map />
+      {err ? <Text>Please enable location services</Text> : null}
+    </SafeAreaView>
   );
 };
 
